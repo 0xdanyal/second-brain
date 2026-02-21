35a12a68af1025921a4ac98e5fd8073a998a072b
Danyal Khan <noreply.github.com>
2026-02-21 21:34:15 +0500

    moved these into server folder

--- src/routes/auth.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { signupLogic, signinLogic } from "../controllers/auth.controller";
-
-const router = Router();
-
-router.post("/signup", signupLogic);
-router.post("/signin", signinLogic);
-
-export default router;
